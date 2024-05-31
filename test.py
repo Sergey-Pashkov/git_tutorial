c5b99c4ac9c598b4fc434a8868fc0b841f7864f4
Vladimir-Mayakovsky <pattern.nsk@gmail.com>
2024-05-31 15:54:03 +0700

    Add datatime

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 name = input("What's your name? ")
 print("Hello, ", name,"!")
 print('We lern Git')
 print('It is three')
 print('It is fourth line')
 
+print ("Today: ",datetime.today().strftime('%d/%m/%Y'))
